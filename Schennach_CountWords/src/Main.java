8732524f7d8049453af470edd4c2e8eb3f2b862d
Chris Schennach <senchb16@new.edvhtl.at>
2019-04-02 19:01:31 +0200

    implemented GUI

--- Schennach_CountWords/src/Main.java
+++ Schennach_CountWords/src/Main.java
@@ -1,7 +1,7 @@
 
 import book.Book;
 import consumer.CountConsumer;
-
+import gui.Zustand;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
 import producer.CountProducer;
@@ -20,11 +20,23 @@ import queue.MyQueue;
 public class Main {
     public static void main(String[] args) {
         MyQueue<Book> queue = new MyQueue(3);
-  
-   
-        CountProducer pro = new CountProducer(queue);
-        CountConsumer con1 = new CountConsumer(queue);
-        CountConsumer con2 = new CountConsumer(queue);
+        JFrame frame = new JFrame();
+        frame.setLayout(new GridLayout(3,1));
+        
+        Zustand producer1 = new Zustand("producer1");
+        frame.add(producer1);
+        
+        Zustand consumer1 = new Zustand("consumer1");
+        frame.add(consumer1);
+        
+        Zustand consumer2 = new Zustand("consumer2");
+        frame.add(consumer2);
+        
+        frame.setVisible(true);
+        frame.setSize(400, 600);
+        CountProducer pro = new CountProducer(queue, producer1);
+        CountConsumer con1 = new CountConsumer(queue, consumer1);
+        CountConsumer con2 = new CountConsumer(queue, consumer2);
         
         pro.start();
         con1.start();
